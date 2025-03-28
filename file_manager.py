a64d6c28c26d55df4ce6a7f6cb03ed870ef4eee2
Fukuro <anya.androschuk@gmail.com>
2025-03-28 12:28:00 +0200

    Added a file read functionality

--- file_manager.py
+++ file_manager.py
@@ -1,3 +1,17 @@
 class FileManager:
     def __init__(self):
         pass
+    
+    def read_file(self, filepath):
+        lines = []
+        
+        with open(filepath, 'r', encoding="utf-8") as file:
+            lines = file.readlines()
+        
+        return lines;
+    
+    # def save_file(self):
+    #     return
+
+    # def filter_file(self):
+    #     return
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from file_manager import FileManager
 def main():
     print('Test manager init')
     manager = FileManager();
-    print(manager);
+    print(manager.read_file('text.txt'));
+
+main()
