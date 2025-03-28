748a7d46eedb4684202a1625df12cb95ff178b1b
Fukuro <anya.androschuk@gmail.com>
2025-03-28 13:46:22 +0200

    fixed styles with autopep8

--- file_manager.py
+++ file_manager.py
@@ -1,28 +1,29 @@
 class FileManager:
     def __init__(self):
         pass
-    
+
     def read_file(self, filepath):
         lines = []
-        
+
         try:
             with open(filepath, 'r', encoding="utf-8") as file:
                 for line in file:
                     clean_line = line.replace("\n", "")
                     lines.append(clean_line)
-                
+
             print(f'File has been successfully read from [{filepath}]')
         except Exception as error:
-            print(f'Unexpected error: {error}');
+            print(f'Unexpected error: {error}')
             return
-        
+
         return lines
-    
+
     def save_file(self, source_lines, target_filepath):
         try:
             if (len(source_lines) == 0):
-                raise Exception(f'The source array is empty, so there is no point in saving something, lol');
-            
+                raise Exception(
+                    f'The source array is empty, so there is no point in saving something, lol')
+
             with open(target_filepath, "w", encoding="utf-8") as file:
 
                 for line in source_lines:
@@ -30,26 +31,25 @@ class FileManager:
 
             print(f'File has been successfully written in [{target_filepath}]')
         except Exception as error:
-            print(f'Unexpected error: {error}');
-        
+            print(f'Unexpected error: {error}')
 
     def filter_lines(self, source_lines, target):
         try:
-            
+
             lines = source_lines
             result = []
-        
+
             for line in lines:
                 if (target in line):
                     clean_line = line.replace("\n", "")
                     result.append(clean_line)
-        
+
             if (len(result) == 0):
-                raise Exception(f'No lines with the "{target}" was found');
-            
+                raise Exception(f'No lines with the "{target}" was found')
+
             print(f'Lines has been successfully filtered [{result}]')
         except Exception as error:
-            print(f'Unexpected error: {error}');
-            return;
-        
+            print(f'Unexpected error: {error}')
+            return
+
         return result
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import sys
 from file_manager import FileManager
 
+
 def middle_test_func(source_filepath, target_filepath, target):
-    manager = FileManager();
+    manager = FileManager()
     lines = manager.read_file(source_filepath)
     filtered_lines = manager.filter_lines(lines, target)
     manager.save_file(filtered_lines, target_filepath)
+
 
 def main():
     SOURCE_FILE = 'public/text.txt'
     TARGET_FILE = 'public/filtered.txt'
     TARGET = 'boo'
     middle_test_func(SOURCE_FILE, TARGET_FILE, TARGET)
+
 
 main()
