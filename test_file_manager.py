cc942950f114a05fe83aefb3df56e4978cd0df7a
Fukuro <anya.androschuk@gmail.com>
2025-03-28 13:44:50 +0200

    added flake

--- test_file_manager.py
+++ test_file_manager.py
@@ -3,13 +3,20 @@ import pytest
 import os
 
 SOURCE_FILE = 'public/text.txt'
-SOURCE_LINES = ['Test line 1', 'Test line boo 2', 'Test line foo 3', 'Test line boo 4']
+SOURCE_LINES = [
+    'Test line 1',
+    'Test line boo 2',
+    'Test line foo 3',
+    'Test line boo 4']
 FILTERED = ['Test line boo 2', 'Test line boo 4']
 TARGET_FILE = 'public/filtered_test.txt'
 
-FILTER_LINES_TEST_PARAMS = [pytest.param(SOURCE_LINES, 'boo', FILTERED, id='positive'),
-                       pytest.param(SOURCE_LINES, 'bruh', None, id='not found'),
-                       ]
+FILTER_LINES_TEST_PARAMS = [pytest.param(SOURCE_LINES, 'boo', FILTERED,
+                            id='positive'),
+                            pytest.param(
+    SOURCE_LINES, 'bruh', None, id='not found'),
+]
+
 
 @pytest.fixture(scope='class', autouse=True)
 def init_manager(request):
@@ -18,22 +25,23 @@ def init_manager(request):
 
 
 class TestFileManager():
-    @pytest.mark.parametrize("source_file,expected_outcome", [(SOURCE_FILE, SOURCE_LINES)])
+    @pytest.mark.parametrize("source_file,expected_outcome",
+                             [(SOURCE_FILE, SOURCE_LINES)])
     def test_read_file(self, source_file, expected_outcome):
         lines = self.manager.read_file(source_file)
         assert lines == expected_outcome
 
-    @pytest.mark.parametrize("source_lines,target, expected_outcome", FILTER_LINES_TEST_PARAMS)
+    @pytest.mark.parametrize("source_lines,target, expected_outcome",
+                             FILTER_LINES_TEST_PARAMS)
     def test_filter_lines(self, source_lines, target, expected_outcome):
         lines = self.manager.filter_lines(source_lines, target)
         assert lines == expected_outcome
 
-    @pytest.mark.parametrize("source_lines,target_file", [(FILTERED, TARGET_FILE)])
+    @pytest.mark.parametrize("source_lines,target_file",
+                             [(FILTERED, TARGET_FILE)])
     def test_save_file(self, source_lines, target_file):
         self.manager.save_file(source_lines, target_file)
         assert os.path.exists(target_file)
         assert self.manager.read_file(target_file) == source_lines
 
         os.remove(target_file)
-    
-    # TO DO: add more parameters if I'll have time 
