c09174fa694f579ea4d7b881ca1d3e728db68702
Fukuro <anya.androschuk@gmail.com>
2025-03-28 12:38:54 +0200

    added filter functionality

--- file_manager.py
+++ file_manager.py
@@ -5,13 +5,29 @@ class FileManager:
     def read_file(self, filepath):
         lines = []
         
-        with open(filepath, 'r', encoding="utf-8") as file:
-            lines = file.readlines()
+        try:
+            with open(filepath, 'r', encoding="utf-8") as file:
+                lines = file.readlines()
+        except Exception as error:
+            print(f'Unexpected error: {error}');
+            return
         
         return lines;
-    
-    # def save_file(self):
-    #     return
 
-    # def filter_file(self):
-    #     return
+    def filter_lines(self, source_lines, target):
+        try:
+            
+            lines = source_lines
+            result = []
+        
+            for line in lines:
+                if (target in line):
+                    result.append(line)
+        
+            if (len(result) == 0):
+                raise Exception(f'No lines with the "{target}" was found');
+        except Exception as error:
+            print(f'Unexpected error: {error}');
+            return;
+        
+        return result
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from file_manager import FileManager
 def main():
     print('Test manager init')
     manager = FileManager();
-    print(manager.read_file('text.txt'));
+    lines = manager.read_file('text.txt')
+    print(manager.filter_lines(lines, 'boo'))
 
 main()
