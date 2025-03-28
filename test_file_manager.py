dd69e8908972c649d2ecb0fc2ebd3ca0aa6f0898
Fukuro <anya.androschuk@gmail.com>
2025-03-28 13:30:45 +0200

    added more test variations

--- test_file_manager.py
+++ test_file_manager.py
@@ -2,6 +2,14 @@ from file_manager import FileManager
 import pytest
 import os
 
+SOURCE_FILE = 'public/text.txt'
+SOURCE_LINES = ['Test line 1', 'Test line boo 2', 'Test line foo 3', 'Test line boo 4']
+FILTERED = ['Test line boo 2', 'Test line boo 4']
+TARGET_FILE = 'public/filtered_test.txt'
+
+FILTER_LINES_TEST_PARAMS = [pytest.param(SOURCE_LINES, 'boo', FILTERED, id='positive'),
+                       pytest.param(SOURCE_LINES, 'bruh', None, id='not found'),
+                       ]
 
 @pytest.fixture(scope='class', autouse=True)
 def init_manager(request):
@@ -10,17 +18,17 @@ def init_manager(request):
 
 
 class TestFileManager():
-    @pytest.mark.parametrize("source_file,expected_outcome", [('public/text.txt', ['Test line 1', 'Test line boo 2', 'Test line foo 3', 'Test line boo 4'])])
+    @pytest.mark.parametrize("source_file,expected_outcome", [(SOURCE_FILE, SOURCE_LINES)])
     def test_read_file(self, source_file, expected_outcome):
         lines = self.manager.read_file(source_file)
         assert lines == expected_outcome
 
-    @pytest.mark.parametrize("source_lines,target, expected_outcome", [(['Test line 1', 'Test line boo 2', 'Test line foo 3', 'Test line boo 4'], 'boo', ['Test line boo 2', 'Test line boo 4',])])
+    @pytest.mark.parametrize("source_lines,target, expected_outcome", FILTER_LINES_TEST_PARAMS)
     def test_filter_lines(self, source_lines, target, expected_outcome):
         lines = self.manager.filter_lines(source_lines, target)
         assert lines == expected_outcome
 
-    @pytest.mark.parametrize("source_lines,target_file", [(['Test line boo 2', 'Test line boo 4',], 'public/filtered_test.txt')])
+    @pytest.mark.parametrize("source_lines,target_file", [(FILTERED, TARGET_FILE)])
     def test_save_file(self, source_lines, target_file):
         self.manager.save_file(source_lines, target_file)
         assert os.path.exists(target_file)
